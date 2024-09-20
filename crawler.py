5adaa0290c44458095a03f000991ab26e20bf937
ChaseS-Actionscript <chasesaltzman07@gmail.com>
2024-09-20 14:29:30 -0400

    Added system arguments to main.py Added file type checking to crawler.py

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ __author__ = "Chase Saltzman"
 __copyright__ = "Copyright (C) 2024 Chase Saltzman"
 __license__ = "Public Domain"
 __version__ = "1.0"
-import requests
+import requests, mimetypes
 from bs4 import BeautifulSoup
 
 class Crawler:
@@ -15,13 +15,13 @@ class Crawler:
         return self.visited
 
     def _visit(self, url):
-        if url not in self.visited:
+        if url not in self.visited and mimetypes.guess_type(url)[0] == mimetypes.guess_type("https://google.com")[0]:
             print(f"Visiting: {url}")
             self.visited.add(url)
+           
             try:
                 response = requests.get(url)
                 soup = BeautifulSoup(response.content, 'html.parser')
-
                 for link in soup.find_all('a', href=True): # <a> tag defines a hyperlink
                     full_url = self._make_full_url(link['href'])
                     if full_url.startswith(self.base_url):
