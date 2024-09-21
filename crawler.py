73d33bf6573c85e949ec7cd664d508def9086a51
Royal51651 <georgeeggers682@gmail.com>
2024-09-21 10:48:36 -0400

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -2,8 +2,13 @@ __author__ = "Chase Saltzman"
 __copyright__ = "Copyright (C) 2024 Chase Saltzman"
 __license__ = "Public Domain"
 __version__ = "1.0"
-import requests
+import requests, mimetypes, threading
 from bs4 import BeautifulSoup
+
+# Speeds for a test website
+# ~12 seconds with default html parser
+# ~10 seconds with lxml
+# ~6 seconds with multithreading
 
 class Crawler:
     def __init__(self, base_url):
@@ -11,47 +16,62 @@ class Crawler:
         self.visited = set()
 
     def crawl(self):
-        self._visit(self.base_url)
+        #self.generate_threads(self.base_url, num_of_threads=3)
+        self._visit(self.base_url) 
         return self.visited
 
     def _visit(self, url):
-        if url not in self.visited:
+        if url not in self.visited and mimetypes.guess_type(url)[0] == mimetypes.guess_type("https://google.com")[0]:
             print(f"Visiting: {url}")
             self.visited.add(url)
+            try:
+                response = requests.get(url)
+                soup = BeautifulSoup(response.content, 'lxml')
+                for link in soup.find_all('a', href=True): # <a> tag defines a hyperlink
+                    full_url = self._make_full_url(link['href'])
+                    if full_url.startswith(self.base_url):
+                        self._visit(full_url)
+            except Exception as e:
+                print(f"Failed to visit {url}: {e}")
 
-            # skip over html parsing if url ends with an invalid file extension because it wont contain any hyperlinks
-            # theres a bunch of other file types that could be found in the <a> tag that I didnt include
-            # this solution still adds the file to the sitemap, so it can be marked as a valid redirect
+    def _parse_html(self, html): # Identical purpose to the crawl function, but passes in html instead of a url for the first go
+        soup = BeautifulSoup(html, 'lxml') # lxml is faster than default html parser
+        for link in soup.find_all('a', href=True):
+            full_url = self._make_full_url(link['href'])
+            if full_url.startswith(self.base_url):
+                self._visit(full_url)
 
-            validExtension = True
-            invalidFileTypes = ['.pdf', '.jpg', '.png', '.gif', '.mp4', '.mp3', '.doc', '.docx', '.xlsx', '.ppt', '.zip', '.rar', '.exe', '.css', '.js', '.xml', '.json']
-            for extension in invalidFileTypes:
-                if(url.endswith(extension)):
-                    print(f'Skipping over {extension} file')
-                    validExtension = False
-                    break
-
-            # might be overkill, but checks the type through the header in case something made it through
-
-            if(validExtension):
-                fileType = requests.head(url)
-                type = fileType.headers.get("Content-Type", "")
-                if('text/html' not in type):
-                    validExtension = False
-                    print(f'Skipping over invalid file')
-
-            # if both checks pass, runs the parser
-
-            if(validExtension):
-                try:
-                    response = requests.get(url)
-                    soup = BeautifulSoup(response.content, 'html.parser')
-                    for link in soup.find_all('a', href=True): # <a> tag defines a hyperlink
-                        full_url = self._make_full_url(link['href'])
-                        if full_url.startswith(self.base_url):
-                            self._visit(full_url)
-                except Exception as e:
-                    print(f"Failed to visit {url}: {e}")
+    def generate_threads(self, url, num_of_threads):
+        threads = []
+        try:
+            response = requests.get(url)
+            html_document = response.text
+            chunks = []
+            iter = 0
+            temp = ""
+            end_tag = False
+            # iterates through the html until the chunk is a good length, and it reaches an ending tag to seperate into a thread
+            for i in html_document:
+                iter += 1
+                temp += i
+                if i == "/":
+                    end_tag = True
+                elif i == "<":
+                    end_tag = False
+                elif iter >= len(html_document) / num_of_threads and i == ">" and end_tag:
+                    chunks.append(temp)
+                    iter = 0
+                    end_chunk = False
+                    temp = ""
+            chunks.append(temp)
+            for chunk in chunks:
+                thread = threading.Thread(target=self._parse_html, args=(chunk,))
+                threads.append(thread)
+                thread.start()
+        except Exception as e:
+            print(f"Failed To Get Html From {url}: {e}")
+        for thread in threads:
+            thread.join()
 
     def _make_full_url(self, link):
         if link.startswith('http'):
