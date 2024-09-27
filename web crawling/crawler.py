a285e4e505c44e762053a146a645839815ea8578
Mocha-Blue <chasesaltzman07@gmail.com>
2024-09-26 22:36:56 -0400

    web crawling algorithm update
    algorithm now outputs in a way that's actually usable by a front end to graph. iteration could still be faster.

--- web crawling/crawler.py
+++ web crawling/crawler.py
@@ -3,33 +3,38 @@ __copyright__ = "Copyright (C) 2024 Chase Saltzman"
 __license__ = "Public Domain"
 __version__ = "1.0"
 import requests, mimetypes
+from typing import List
 from bs4 import BeautifulSoup
-
+"""
+TODO
+lxml - etree might be the solution to faster parsing current iteration is already fast but it could be faster
+lxml is mostly used for xml though
+also xml has a vulnerability that has to do with not disabling dtd entity expansion
+"""
 class Crawler:
     def __init__(self, base_url):
         self.base_url = base_url
         self.visited = set()
-
-    def crawl(self) -> set:
+        self.edges: List[tuple] = []
+        
+    def crawl(self):
         self._visit(self.base_url)
-        return self.visited
+        return self.edges  
 
     def _visit(self, url):
         if url not in self.visited and mimetypes.guess_type(url)[0] == mimetypes.guess_type("https://google.com")[0]:
             print(f"Visiting: {url}")
             self.visited.add(url)
-           
             try:
                 response = requests.get(url)
-                soup = BeautifulSoup(response.content, 'html.parser')
-                for link in soup.find_all('a', href=True): # find all <a> tags that have an href attribute (links)
+                soup = BeautifulSoup(response.content, 'html.parser') 
+                for link in soup.find_all('a', href=True):
                     full_url = self._make_full_url(link['href'])
-                    if full_url.startswith(self.base_url): # this is a hacky way to skip any ad links by simply preventing any links to other websites
-                        self._visit(full_url)
+                    self.edges.append((url, full_url))
             except Exception as e:
                 print(f"Failed to visit {url}: {e}")
 
     def _make_full_url(self, link):
         if link.startswith('http'):
             return link
-        return f"{self.base_url}/{link.lstrip('/')}" # create full url by combining base url with the relative path
+        return f"{self.base_url}/{link.lstrip('/')}"
